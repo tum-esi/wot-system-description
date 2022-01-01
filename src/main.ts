e13235632bff369b2c5ba309a20e1b8d4f1e82c2
fatadel <fatadel@gmail.com>
2022-01-01 21:55:26 +0100

    feat: add inquirer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-const args = process.argv.slice(2)
-
-import fs = require( "fs" )
+import fs = require("fs")
 import checkSD from "./validateSd"
 import { SQSD } from "../definitions/defs"
 import parseSeqD from "./parseSeqD"
@@ -9,141 +7,189 @@ import generateSeqD from "./generateSeqD"
 import generateSD from "./generateSD"
 import generateTS from "./codeGen"
 import validateSeqD from "./validateSeqD"
+const inquirer = require("inquirer");
 
+const questions = [
+    {
+        type: "list",
+        name: "generationType",
+        message: "What's your desired generation type?",
+        choices: ["Sequence Diagram -> System Description", "System Description -> Sequence Diagram"],
+        filter(val) {
+            if (val === "Sequence Diagram -> System Description") {
+                return 0;
+            }
+            return 1;
+        },
+    },
+    {
+        type: "input",
+        name: "SeqDPath",
+        message: "Path to folder containing your Sequence Diagram(s) (relative to this directory)",
+        default: "./example-input/SeqD-examples/SeqDs",
+        when(answers) {
+            return answers.generationType === 0;
+        },
+    },
+    {
+        type: "input",
+        name: "TDsPath",
+        message: "Path to Thing Descriptions (relative to this directory)",
+        default: "./example-input/SeqD-examples/TDs.json",
+        when(answers) {
+            return answers.generationType === 0;
+        },
+    },
+    {
+        type: "input",
+        name: "SDPath",
+        message: "Path to folder containing your System Description(s) (relative to this directory)",
+        default: "./example-input/SD-examples/SDs",
+        when(answers) {
+            return answers.generationType === 1;
+        },
+    },
+    {
+        type: "input",
+        name: "outputPath",
+        message: "Where to put created output? (relative to this directory)",
+        default: "./created-output",
+    },
+];
 
-const SDCHECK = true
-// used expressions: TODO - important, OPT - improvement
-// determine filePaths
+inquirer.prompt(questions).then((answers) => {
+    const SDCHECK = true
+    // used expressions: TODO - important, OPT - improvement
+    // determine filePaths
 
-let inFilesPath = args.shift()?.replace(/\\/g, "/") ?? "./example-input/SeqD-examples/SeqDs"
-if (inFilesPath.endsWith("/")) {
-    inFilesPath = inFilesPath.slice(0,-1)
-}
-const tdFilePath = args.shift() ?? "./example-input/SeqD-examples/TDs.json"
-const outputPath = "./created-output" + inFilesPath.replace(/^[^]*example-input/, "").split("/").slice(0,-1).join("/") + "/"
+    let inFilesPath = (answers.SeqDPath ?? answers.SDPath).replace(/\\/g, "/")
+    if (inFilesPath.endsWith("/")) {
+        inFilesPath = inFilesPath.slice(0, -1)
+    }
+    const tdFilePath = answers.TDsPath
+    const outputPath = answers.outputPath + inFilesPath.replace(/^[^]*example-input/, "").split("/").slice(0, -1).join("/") + "/"
 
-// createFolders
-fs.mkdirSync(outputPath + "SeqDs/", {recursive: true})
-fs.mkdirSync(outputPath + "SDs/", {recursive: true})
-fs.mkdirSync(outputPath + "Code/", {recursive: true})
-if(!fs.existsSync(outputPath + "tsconfig.json")) {
-    fs.copyFileSync("./definitions/tsconfig.json", outputPath + "tsconfig.json")
-}
+    // createFolders
+    fs.mkdirSync(outputPath + "SeqDs/", { recursive: true })
+    fs.mkdirSync(outputPath + "SDs/", { recursive: true })
+    fs.mkdirSync(outputPath + "Code/", { recursive: true })
+    if (!fs.existsSync(outputPath + "tsconfig.json")) {
+        fs.copyFileSync("./definitions/tsconfig.json", outputPath + "tsconfig.json")
+    }
 
-// ------------ Program -------------
-fs.readdir(inFilesPath, (err, fileNames) => {
-    if (err) {throw new Error("could not read input files")}
-    fileNames.forEach( thisFileName => {
-        const filePath = inFilesPath + "/" + thisFileName
-        const fileType = thisFileName.split(".").pop()
-        const outFileName = thisFileName.split(".").slice(0,-1).join(".")
-        const inFile = fs.readFileSync(filePath, "utf8")
-        let outFile: string
-        let wholeOutPath: string
-        let mashupLogic: SQSD.mashupLogic | undefined
+    // ------------ Program -------------
+    fs.readdir(inFilesPath, (err, fileNames) => {
+        if (err) { throw new Error("could not read input files") }
+        fileNames.forEach(thisFileName => {
+            const filePath = inFilesPath + "/" + thisFileName
+            const fileType = thisFileName.split(".").pop()
+            const outFileName = thisFileName.split(".").slice(0, -1).join(".")
+            const inFile = fs.readFileSync(filePath, "utf8")
+            let outFile: string
+            let wholeOutPath: string
+            let mashupLogic: SQSD.mashupLogic | undefined
 
-        let outCode: string
-        let outBase: string
-        let codeInput: string | undefined
+            let outCode: string
+            let outBase: string
+            let codeInput: string | undefined
 
-        if (fileType === "json") {
-            // convert SD to Sequence Diagram
+            if (answers.generationType == 1 && fileType === "json") {
+                // convert SD to Sequence Diagram
 
-            checkSD(inFile, SDCHECK).then( () => {
+                checkSD(inFile, SDCHECK).then(() => {
 
-                const inSD: SQSD.sdTemplate = JSON.parse(inFile)
-                codeInput = inFile
-                wholeOutPath = outputPath + "SeqDs/" + outFileName + ".puml"
-                const outTDs: SQSD.subthing[] = []
-                Object.keys(inSD.things).forEach( name => {
-                    outTDs.push(inSD.things[name])
-                })
-
-                try {
-                    mashupLogic = parseSD(inSD)
-                } catch (error) {
-                    console.error({thisFileName})
-                    throw new Error({thisFileName} + " parse SD problem!: " + error)
-                }
-                try {
-                    outFile = generateSeqD(mashupLogic)
-                } catch (error) {
-                    console.error({thisFileName})
-                    throw new Error({thisFileName} + " generate SeqD problem!: " + error)
-                }
-                codeGen()
-
-                fs.writeFile(wholeOutPath, outFile, "utf8", () => {
-                    fs.writeFile(outputPath + "TDs.json", JSON.stringify(outTDs), () => {
-                        console.log({thisFileName, "->": "conversion done!"})
+                    const inSD: SQSD.sdTemplate = JSON.parse(inFile)
+                    codeInput = inFile
+                    wholeOutPath = outputPath + "SeqDs/" + outFileName + ".puml"
+                    const outTDs: SQSD.subthing[] = []
+                    Object.keys(inSD.things).forEach(name => {
+                        outTDs.push(inSD.things[name])
                     })
-                })
-            }, e => {
-               console.error({thisFileName}, "input SD invalid: " + e)
-            })
 
-        } else if (fileType === "puml") {
-            // convert Sequence Diagram to SD
-            validateSeqD(inFile).then( ok => {
-                console.log({thisFileName},{ok})
-                wholeOutPath = outputPath + "SDs/" + outFileName + ".json"
+                    try {
+                        mashupLogic = parseSD(inSD)
+                    } catch (error) {
+                        console.error({ thisFileName })
+                        throw new Error({ thisFileName } + " parse SD problem!: " + error)
+                    }
+                    try {
+                        outFile = generateSeqD(mashupLogic)
+                    } catch (error) {
+                        console.error({ thisFileName })
+                        throw new Error({ thisFileName } + " generate SeqD problem!: " + error)
+                    }
+                    codeGen()
 
-                const inTds = fs.readFileSync(tdFilePath, "utf8")
-                try {
-                    mashupLogic = parseSeqD(inFile)
-                } catch (error) {
-                    console.error({thisFileName})
-                    throw new Error(thisFileName + " parseSeqD problem!: " + error)
-                }
-
-                try {
-                    outFile = generateSD(mashupLogic, inTds)
-                } catch (error) {
-                    console.error({thisFileName})
-                    throw new Error(thisFileName + " generateSD problem!: " + error)
-                }
-
-                codeInput = outFile
-
-                checkSD(outFile, SDCHECK).then( () => {
                     fs.writeFile(wholeOutPath, outFile, "utf8", () => {
-                        console.log({thisFileName, "->": "conversion done!"})
+                        fs.writeFile(outputPath + "TDs.json", JSON.stringify(outTDs), () => {
+                            console.log({ thisFileName, "->": "conversion done!" })
+                        })
                     })
                 }, e => {
-                    fs.writeFile(wholeOutPath, outFile, "utf8", () => {
-                        console.error({thisFileName}, "created output SD is invalid: " + e)
+                    console.error({ thisFileName }, "input SD invalid: " + e)
+                })
+
+            } else if (answers.generationType == 0 && fileType === "puml") {
+                // convert Sequence Diagram to SD
+                validateSeqD(inFile).then(ok => {
+                    console.log({ thisFileName }, { ok })
+                    wholeOutPath = outputPath + "SDs/" + outFileName + ".json"
+
+                    const inTds = fs.readFileSync(tdFilePath, "utf8")
+                    try {
+                        mashupLogic = parseSeqD(inFile)
+                    } catch (error) {
+                        console.error({ thisFileName })
+                        throw new Error(thisFileName + " parseSeqD problem!: " + error)
+                    }
+
+                    try {
+                        outFile = generateSD(mashupLogic, inTds)
+                    } catch (error) {
+                        console.error({ thisFileName })
+                        throw new Error(thisFileName + " generateSD problem!: " + error)
+                    }
+
+                    codeInput = outFile
+
+                    checkSD(outFile, SDCHECK).then(() => {
+                        fs.writeFile(wholeOutPath, outFile, "utf8", () => {
+                            console.log({ thisFileName, "->": "conversion done!" })
+                        })
+                    }, e => {
+                        fs.writeFile(wholeOutPath, outFile, "utf8", () => {
+                            console.error({ thisFileName }, "created output SD is invalid: " + e)
+                        })
+                    })
+                    codeGen()
+                }, notOk => {
+                    console.log({ thisFileName }, notOk)
+                    throw new Error("!!! invalid Sequence Diagram notation" + notOk)
+                })
+
+
+            }
+
+            function codeGen() {
+                // Code generation
+                const codeOutPath = outputPath + "Code/" + outFileName
+                if (!mashupLogic) { throw new Error("problem with mashup logic!") }
+                if (!codeInput) { throw new Error("sd generation not working?") }
+                try {
+                    const genResult = generateTS(JSON.parse(codeInput), mashupLogic, outFileName)
+                    outCode = genResult.code
+                    outBase = genResult.base
+                } catch (error) {
+                    console.error({ thisFileName })
+                    throw new Error(thisFileName + " codeGen problem!: " + error)
+                }
+                fs.writeFile((codeOutPath + "_index.js"), outBase, "utf8", () => {
+                    fs.writeFile((codeOutPath + ".ts"), outCode, "utf8", () => {
+                        console.log({ thisFileName, "->": "code generation done!" })
                     })
                 })
-                codeGen()
-            }, notOk => {
-                console.log({thisFileName}, notOk)
-                throw new Error("!!! invalid Sequence Diagram notation" + notOk)
-            })
-
-
-        } else {
-            throw new Error("unknown filetype as input at " + {thisFileName})
-        }
-
-        function codeGen() {
-            // Code generation
-            const codeOutPath = outputPath + "Code/" + outFileName
-            if(!mashupLogic) {throw new Error("problem with mashup logic!")}
-            if(!codeInput) {throw new Error("sd generation not working?")}
-            try {
-                const genResult = generateTS(JSON.parse(codeInput), mashupLogic, outFileName)
-                outCode = genResult.code
-                outBase = genResult.base
-            } catch (error) {
-                console.error({thisFileName})
-                throw new Error(thisFileName + " codeGen problem!: " + error)
             }
-            fs.writeFile((codeOutPath + "_index.js"), outBase, "utf8", () => {
-                fs.writeFile((codeOutPath + ".ts"), outCode, "utf8", () => {
-                    console.log({thisFileName, "->": "code generation done!"})
-                })
-            })
-        }
 
+        })
     })
-})
+});
