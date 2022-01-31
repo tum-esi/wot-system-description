17ead2df8cd8c15a4611aa09d60c13e9ecc334bb
fatadel <fatadel@gmail.com>
2022-01-31 17:10:37 +0100

    feat: populate all other fields after processing system description

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -19,12 +19,32 @@ processSD.addEventListener('click', async (e) => {
         const puml = await response.text();
         document.querySelector('#puml-text textarea').value = puml;
 
-        document.querySelector('#puml-rendered div').innerHTML = `<img src="https://www.plantuml.com/plantuml/img/${window.plantumlEncoder.encode(puml)}">`;
+        document.querySelector('#puml-rendered div').innerHTML =
+            `<img src="https://www.plantuml.com/plantuml/img/${window.plantumlEncoder.encode(puml)}">`;
+
+        path = data['tds'];
+        response = await fetch(path, {
+            method: 'GET'
+        });
+        document.querySelector('#tds textarea').value =
+            JSON.stringify(JSON.parse(await response.text()), null, 4);
 
         path = data['code']['js'];
         response = await fetch(path, {
             method: 'GET'
         });
         document.querySelector('#js-code textarea').value = await response.text();
+
+        path = data['code']['ts'];
+        response = await fetch(path, {
+            method: 'GET'
+        });
+        document.querySelector('#ts-code textarea').value = await response.text();
+
+        path = data['code']['tsconfig'];
+        response = await fetch(path, {
+            method: 'GET'
+        });
+        document.querySelector('#tsconfig textarea').value = await response.text();
     }
 });
